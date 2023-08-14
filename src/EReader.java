451d2b59eef18e6da6bd2b139f77ca2a11620fee
Emilie Beck <98420041+themilkprince@users.noreply.github.com>
2023-08-14 12:09:46 -0600

    Update EReader.java

--- src/EReader.java
+++ src/EReader.java
@@ -122,10 +122,10 @@ public class EReader {
 
     public static void main(String[] args){
         ArrayList<String> myBooks = new ArrayList<>();
-        myBooks.add("src/Great_Gatsby.txt");
-        myBooks.add("src/Alice_In_Wonderland.txt");
-        myBooks.add("src/US_Constitution.txt");
-        myBooks.add("src/equations.txt");
+        myBooks.add("Great_Gatsby.txt");
+        myBooks.add("Alice_In_Wonderland.txt");
+        myBooks.add("US_Constitution.txt");
+        myBooks.add("equations.txt");
         EReader myBrokenEreader = new EReader(myBooks);
         myBrokenEreader.go();
     }
